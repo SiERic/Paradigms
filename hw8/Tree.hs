4060e4b1e282bfde96c81a97236c40fb2c448710
Alexandra Olemskaya <olemskaya@inbox.ru>
2017-11-03 20:06:22 +0300

    fix depth problem

--- hw8/Tree.hs
+++ hw8/Tree.hs
@@ -8,7 +8,7 @@ data BinaryTree k v = Nil | Node k v (BinaryTree k v) (BinaryTree k v) deriving 
 -- Takes a key and a tree and returns Just value if the given key is present,
 -- otherwise returns Nothing
 lookup :: Ord k => k -> BinaryTree k v -> Maybe v
-lookup key Nil = Nothing
+lookup _ Nil = Nothing
 lookup key (Node k v l r) | key < k   = lookup key l
                           | key == k  = Just v
                           | key > k   = lookup key r
@@ -21,13 +21,23 @@ insert key value (Node k v l r) | key < k   = Node k v (insert key value l) r
                                 | key == k  = Node k v l r
                                 | key > k   = Node k v l (insert key value r) 
 
-merge :: BinaryTree k v -> BinaryTree k v -> BinaryTree k v
-merge Nil t = t
-merge (Node k v l r) t = Node k v l (merge r t)
+extract_min :: BinaryTree k v -> (BinaryTree k v, (k, v))
+extract_min Nil = (Nil, (undefined, undefined))
+extract_min (Node k v Nil r) = (r, (k, v))
+extract_min (Node k v l r)  = ((Node k v new_tree r), new_root)
+                               where new_root = snd (extract_min l)
+                                     new_tree = fst (extract_min l)                           
 
 -- Returns a new tree without the given key
 delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
-delete key Nil = Nil 
+delete _ Nil = Nil 
+delete key (Node k v Nil Nil) | key == k   = Nil
+                              | otherwise  = (Node k v Nil Nil)
+delete key (Node k v l Nil) | key < k   = Node k v (delete key l) Nil
+	                        | key == k  = l
+                            | otherwise = (Node k v l Nil)
 delete key (Node k v l r) | key < k   = Node k v (delete key l) r
-                          | key == k  = merge l r
+                          | key == k  = (Node (fst new_root) (snd new_root) l new_tree)
                           | key > k   = Node k v l (delete key r)
+                          where new_root = snd (extract_min r)
+                                new_tree = fst (extract_min r)  
